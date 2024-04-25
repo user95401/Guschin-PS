ae67c1fc9040f39430e511ad249f409de35ead6e
user95401 <user95401@gmail.com>
2024-04-25 16:26:01 +0400

    GEODE_IS_WINDOWS

--- src/window-name.cpp
+++ src/window-name.cpp
@@ -6,8 +6,10 @@ using namespace geode::prelude;
 #include <Geode/modify/CCScene.hpp>
 class $modify(CCScene) {
     static CCScene* create() {
+#ifdef GEODE_IS_WINDOWS
         auto GameWindowHandle = WindowFromDC(MBO(HDC, CCEGLView::sharedOpenGLView()->getWindow(), 0x244));
         SetWindowTextA(GameWindowHandle, Mod::get()->getName().data());
+#endif
         return CCScene::create();
     }
 };
